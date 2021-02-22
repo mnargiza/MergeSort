375e8d1fd6ea3e3966cc000034bf08face2eab71
mnargiza <55973452+mnargiza@users.noreply.github.com>
2021-02-22 18:54:44 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -3,8 +3,25 @@
 #include <iostream>
 #include <vector>
 #include <boost/thread.hpp>
+#include <math.h>
+#include <time.h>
+
+
 
 using namespace std;
 
+const int amountOfTthreads = 4;
+
 void Sort(double arr[], int n, double* tmp);
-void Merge(double arr1[], int n1, float arr2[], int n2, double* arr3);
+void Merge(double arr1[], int n1, double arr2[], int n2, double* arr3, int j);
+
+// Merge Sort
+void task_1(double arr[], int n, double* res);
+
+// Merge Sort with threads
+void task_2(double arr[], int n, double* tmp);
+
+void ParallelSort(double arr[], int size, double* tmp, int iter, int rem);
+void ParallelMerge(double arr[], boost::thread threads[], double* res, int n, int step, int remainder);
+
+void test();
