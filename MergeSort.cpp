581380c8abbec042967c00a27eecb977e7df746f
mnargiza <55973452+mnargiza@users.noreply.github.com>
2021-02-22 15:13:36 +0500

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,22 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void Merge(double arr1[], int n1, double arr2[], int n2, double* arr3)
-{
-    int i1 = 0, i2 = 0, j = 0; //i1- индекс arr1[], i2-индекс arr2[] , j-индекс полученного отсортиров. 2 массивов
-    for (i1 = 0, i2 = 0, j = 0; i1 < n1 && i2 < n2;)
-    {
-        if (arr1[i1] < arr2[i2])
-        {
-            arr3[j++] = arr1[i1++];
-        }
-        else
-            arr3[j++] = arr2[i2++];
-        memcpy(arr3 + j, arr1 + i1, (n1 - i1) * sizeof(double));
-        memcpy(arr3 + j, arr2 + i2, (n2 - i2) * sizeof(double));
-    }
-}
-void Sort(double arr[], int n, double* tmp) //делим массив на 2 части, потом эти 2 части еще на 2 и т.д
+
+void Sort(double arr[], int n, double* tmp)
 {
     int l1, l2;
     if (n <= 1) return;
@@ -28,9 +14,25 @@ void Sort(double arr[], int n, double* tmp) //делим массив на 2 части, потом эти
     l2 = n - l1;
     Sort(arr, l1, tmp);
     Sort(arr + l1, l2, tmp);
-    Merge(arr, l1, arr + l1, l2, tmp);// cортируем каждый часть длины не меньше 1
-    memcpy(arr, tmp, n * sizeof(double));//
+    Merge(arr, l1, arr + l1, l2, tmp, 0);
+    memcpy(arr, tmp, n * sizeof(double));
 }
 
+void Merge(double arr1[], int n1, double arr2[], int n2, double* arr3, int pos)
+{
+    int i1 = 0, i2 = 0, j; //i1- position of arr1[], i2-position of arr2[] , j-position where to start merging
+    for (i1 = 0, i2 = 0, j=pos; i1 < n1 && i2 < n2;)
+    {
+        if (arr1[i1] < arr2[i2])
+        {
+            arr3[j++] = arr1[i1++];
+        }
+        else
+            arr3[j++] = arr2[i2++];
+     
+        memcpy(arr3 + j, arr1 + i1, (n1 - i1) * sizeof(double));
+        memcpy(arr3 + j, arr2 + i2, (n2 - i2) * sizeof(double));
+    }
+}
 
 
