bd7388724bf703380cabdeddceb0ea3982102cf3
mnargiza <55973452+mnargiza@users.noreply.github.com>
2021-03-24 00:51:09 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -5,12 +5,11 @@
 #include <boost/thread.hpp>
 #include <math.h>
 #include <time.h>
-
-
+#include <utility>
 
 using namespace std;
 
-const int amountOfTthreads = 2;
+const int amountOfThreads = 4;
 
 void Sort(double arr[], int n, double* tmp);
 void Merge(double arr1[], int n1, double arr2[], int n2, double* arr3, int j);
@@ -21,7 +20,7 @@ void task_1(double arr[], int n, double* res);
 // Merge Sort with threads
 void task_2(double arr[], int n, double* tmp);
 
-void ParallelSort(double arr[], int size, double* tmp, int iter, int rem);
-void ParallelMerge(double arr[], boost::thread threads[], double* res, int n, int step, int remainder);
+void sort_with_threads(double arr[], int cur_thread, int step, double* tmp);
+pair< int, int > memory_distribution(int step, int cur_thread, int i);
 
 void test();
