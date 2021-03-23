1128932f9b55e827e8857a9b771e83ae064c126c
mnargiza <55973452+mnargiza@users.noreply.github.com>
2021-03-24 00:50:02 +0500

    Update ParallelMergeSort.cpp

--- ParallelMergeSort.cpp
+++ ParallelMergeSort.cpp
@@ -1,51 +1,12 @@
 #include "Header.h"
 
+boost::mutex m;
 
-
-
-void ParallelSort(double arr[], int size, double* tmp, int iter, int rem) 
-{
-	int l1, l2;
-
-	if (size <= 1) 
-		return;
-
-	l1 = size / 2;
-	l2 = size - l1;
-	ParallelSort(arr, l1, tmp + iter * size, iter, rem);
-	ParallelSort(arr + l1, l2, tmp + iter * size, iter, rem);
-	Merge(arr, l1, arr + l1, l2, tmp + iter * size,0);
-	memcpy(arr, tmp+ iter*size, size * sizeof(double));
-}
-
-void ParallelMerge(double arr[], boost::thread threads[], double* res, int n, int step, int remainder)
-{
-	//Merge with threads
-
-	for (int j = 0; j < ceil(amountOfTthreads / 2); j++) {
-		for (int i = j; i < amountOfTthreads; i ++) {
-			if (i >= 1 && i % 2 != 0 && i * step * (j + 1) < n) {
-				if (i == amountOfTthreads - 1 && remainder != 0)
-					threads[i-1] = boost::thread(Merge, arr + (i - 1) * step, step*(j+1), arr + i * step * (j + 1) + remainder, step * (j + 1) + remainder, ref(res), (i - 1) * step);
-				else
-					threads[i-1] = boost::thread(Merge, arr + (i - 1) * step, step * (j + 1), arr + i * step * (j + 1), step * (j + 1), ref(res), (i - 1) * step);
-				
-			}
-			
-		}
-
-		for (int i = 0; i < amountOfTthreads-2*j; i=i+2)
-			threads[i].join();
-		memcpy(arr, res, n * sizeof(double));
-	}
-
-}
 
 // Merge Sort
-void task_1(double arr[], int n, double* res){
+void task_1(double arr[], int n, double* res) {
 	Sort(arr, n, res);
 
-	
 	/*for (int i = 0; i < n; i++) {
 		if (n == 1) {
 			cout << arr[0] << ' ';
@@ -56,11 +17,70 @@ void task_1(double arr[], int n, double* res){
 
 }
 
+
+//распределение памяти для каждого треда
+//pair< int, int > data; начало и конец блока памяти
+// i - номер итерации
+pair< int, int > memory_distribution(int step, int cur_thread, int i)
+{
+	pair< int, int > data;
+	int s;
+	s = cur_thread * step;
+	data.first = s;
+	data.second = s + pow(2, i) * step - 1;
+
+	return data;
+}
+
+
+void sort_with_threads(double arr[], int cur_thread, int step, double* tmp)
+{
+	pair< int, int > block;
+	int i = 0;
+	if (cur_thread % 2 != 0) {
+		block = memory_distribution(step, cur_thread, i);
+		
+		/*cout << "cur_thread = " << cur_thread << " " << "iter = " << i << endl;
+		cout << block.first << " ";
+		cout << block.second << endl;
+		cout << "size = " << pow(2, i) * step << endl;*/
+		Sort(arr + block.first, pow(2, i) * step, tmp);
+	
+	}
+	else {
+		for (int i = 0; i < log2(amountOfThreads); i++) {
+			block = memory_distribution(step, cur_thread, i);
+			int size = pow(2, i) * step;
+			/*cout << "cur_thread = " << cur_thread << " " << "iter = " << i << endl;
+			cout << block.first << " ";
+			cout << block.second << endl;
+			cout << "size = " << size << endl;*/
+			
+			if (size == step)
+				Sort(arr + block.first, pow(2, i) * step, tmp);
+			else {
+				m.lock();
+				
+				//cout << "Merge" << endl;
+
+				Sort(arr + block.first, pow(2, i) * step, tmp);
+				/*Merge(arr + block.first, size, arr + block.first+size, size, tmp, block.first);
+				for (i = 0; i < size *2; i++)
+					arr[i + block.first] = tmp[i + block.first];*/
+				//cout << "copied" << endl;
+				m.unlock();
+				
+			}	
+			
+		}
+		
+	}
+}
+
 // Merge Sort with threads
 void task_2(double arr[], int n, double* tmp) {
-	
 
-	if (n < amountOfTthreads) {
+	if (n < amountOfThreads) {
 		Sort(arr, n, tmp);
 		/*for (int i = 0; i < n; i++) {
 			if (n == 1) {
@@ -74,63 +94,33 @@ void task_2(double arr[], int n, double* tmp) {
 	}
 
 	int step, remainder;
-	step = n / amountOfTthreads;
-	remainder = n % amountOfTthreads;
-	//cout << "step = " << step << "   remainder = " << remainder << endl;
-	boost::thread threads[amountOfTthreads];
+	step = n / amountOfThreads;
+	
+	boost::thread threads[amountOfThreads];
 
-	double* result = new double[n];
-	if (n == amountOfTthreads) {
-		ParallelMerge(arr, threads, ref(result), n, step, remainder);
+	for (int i = 0; i < amountOfThreads; i++) {
+
+		threads[i] = boost::thread(sort_with_threads, arr, i, step, ref(tmp));
+
 	}
-	else {
 
-		for (int i = 0; i < amountOfTthreads; i++) {
+	for (int i = 0; i < amountOfThreads; i++)
+		threads[i].join();
 
-			double* a;
-
-			if (i == amountOfTthreads - 1 && remainder != 0) {
-				a = new double[step + remainder];
-				memcpy(a, arr + i * step, (step + remainder) * sizeof(double));
-				threads[i] = boost::thread(ParallelSort, a, step + remainder, ref(tmp), i, remainder);
-				///cout << "массив эл:\n";
-							/*for (int i = 0; i < step+remainder; i++)
-								cout << a[i] << " ";
-							cout << endl;*/
-
-			}
-			else {
-				a = new double[step];
-
-				memcpy(a, arr + i * step, step * sizeof(double));
-
-				/*cout << "массив эл:\n";
-				for (int i = 0; i < step; i++)
-					cout << a[i] << " ";
-				cout << endl;*/
-
-
-
-				threads[i] = boost::thread(ParallelSort, a, step, ref(tmp), i, remainder);
-
-			}
-		}
-
-
-		for (int i = 0; i < amountOfTthreads; i++)
-			threads[i].join();
-		//cout << "Merging" << endl;
-
-		ParallelMerge(tmp, threads, ref(result), n, step, remainder);
+	//cout << "last step" << endl;
+	
+	Merge(arr, n / 2, arr + n / 2, n-n / 2, tmp, 0);
+	for (int i = 0; i < n; i++) {
+		arr[i] = tmp[i];
+		//cout << arr[i]<<" ";
 	}
-	/*for (int i = 0; i < n; i++)
-		cout << result[i] << ' ';*/
+	
 }
 int main(int argc, char* argv[])
 {
 	setlocale(LC_CTYPE, "rus");
-
+	
 	test();
-
+	
 	return 0;
 }
