98e32d33f55565d4cfed3ad75ef35ec696d88e89
mnargiza <55973452+mnargiza@users.noreply.github.com>
2021-04-07 00:20:36 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -6,10 +6,37 @@
 #include <math.h>
 #include <time.h>
 #include <utility>
-
+#include <iomanip>
 using namespace std;
 
-const int amountOfThreads = 4;
+
+class barrier {
+    int threadCount;
+    std::atomic<unsigned int>threadsWaiting;
+    bool isNotWaiting;
+    std::condition_variable waitVariable;
+    std::mutex mutex;
+public:
+    barrier(unsigned int n) : threadCount(n) {
+        threadsWaiting = 0;
+        isNotWaiting = false;
+    }
+    barrier(const barrier&) = delete;
+    void wait() {
+        if (threadsWaiting.fetch_add(1) >= threadCount - 1) {
+            isNotWaiting = true;
+            waitVariable.notify_all();
+            threadsWaiting.store(0);
+        }
+        else {
+            std::unique_lock<std::mutex> lock(mutex);
+            waitVariable.wait(lock, [&] { return isNotWaiting; });
+        }
+    }
+    void reduce() {
+        threadCount = threadCount - 2;
+    }
+};
 
 void Sort(double arr[], int n, double* tmp);
 void Merge(double arr1[], int n1, double arr2[], int n2, double* arr3, int j);
@@ -18,9 +45,10 @@ void Merge(double arr1[], int n1, double arr2[], int n2, double* arr3, int j);
 void task_1(double arr[], int n, double* res);
 
 // Merge Sort with threads
-void task_2(double arr[], int n, double* tmp);
+void task_2(double arr[], int n, double* tmp, int amountOfThreads);
 
-void sort_with_threads(double arr[], int cur_thread, int step, double* tmp);
+void sort_with_threads(double arr[], int cur_thread, int step, double* t, int amountOfThreads);
 pair< int, int > memory_distribution(int step, int cur_thread, int i);
 
 void test();
+
