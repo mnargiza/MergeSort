861bce1f36df5e050ca5664e8c4cba70f5efc0be
mnargiza <55973452+mnargiza@users.noreply.github.com>
2021-04-07 00:17:35 +0500

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,45 +1,61 @@
 #include "Header.h"
 
 void test() {
-	int numberOfTests = 8; // amounts of tests
-	int k = 5;
+	int amountOfThreads, t=0;
+	int numberOfTests = 10; // amounts of tests
+	int k = 8;
+	bool IsRight;
 	clock_t start_time, end_time, time;
-
-
 	for (int i = 0; i < numberOfTests; i++) {
+	
 		double* arr = new double[k];
 		double* tmp = new double[k];
+		double* arr2 = new double[k];
+		double* arr3 = new double[k];
 		for (int j = 0; j < k; j++) {
-			arr[j] = rand() % k;
+				arr[j] = rand() % k;
+				arr2[j] = arr[j];
+				arr3[j] = arr[j];
 		}
-		
-		cout << "Êîëè÷åñòâî ÷èñåë: "<<k<<endl;
-
-		/*for (int i = 0; i < k; i++) {
-			cout << arr[i] << ' ';
-		}*/
-
-		//cout << endl;
-		//cout << "\nÏîñëåäîâàòåëüíàÿ ñîðòèðîâêà: " << endl;
+			
+		cout << "Количество чисел: " << k << endl;
+		//cout << setw(10) <<"Количество чисел "<< setw(10) << "2 " << setw(10) << "4 " << setw(10) << "8 " << setw(10) << "Время " << endl;
+	
+		//cout << "\nПоследовательная сортировка: " << endl;
 
 		start_time = clock();
 		task_1(arr, k, tmp);
 		end_time = clock();
 		time = end_time - start_time;
-		cout << "\nTime of sequential sort: " << (double)time/ CLOCKS_PER_SEC << "  \n";
+		cout << "Time of sequential sort: " << (double)time / CLOCKS_PER_SEC << endl;
+		for (int l = 2; l <=16; l=l*2) {
+			amountOfThreads = l;
+			
+			
+		
 
-		//cout << endl;
-		//cout << "\nÏàðàëëåëüíàÿ ñîðòèðîâêà: " << endl;
+			//
+			/*for (int i = 0; i < k; i++) {
+				 tmp[i]=0;
+			}*/
+			//cout << "\nПараллельная сортировка: " << endl;
+			cout << "Number of threads: " << amountOfThreads << endl;
 
-		start_time = clock();
-		task_2(arr, k, tmp);
-		end_time = clock();
-		time = end_time - start_time;
-		cout << "\nTime parallel sort: " << (double)time / CLOCKS_PER_SEC << "  \n";
-		//ParallelSort(arr, k, tmp);
-		//cout << arr[i] << ' ';
-		k = k * 4;
+			if (l > 2) {
+				for (int j = 0; j < k; j++) {
+					arr2[j] = arr3[j];
+				}
+			}
+
+			start_time = clock();
+			task_2(arr2, k, tmp, amountOfThreads);
+			end_time = clock();
+			time = end_time - start_time;
+			cout << "Time parallel sort: " << (double)time / CLOCKS_PER_SEC << endl;
+		
+			
+		}
+		//cout << setw(10) << k << setw(10) << l << setw(10) << "Время " << endl;
+		k = k * 2;
 	}
-
-
 }
